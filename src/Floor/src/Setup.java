e5d287bfa38e148bf18d8a082f4dd68a39018bc5
zuoyzhao <zuoyzhao@CS-S120.iowa.uiowa.edu>
2016-11-04 14:44:40 -0500

    floor: fifth try(3rd try skipped because of some stupid mistakes)

--- src/Floor/src/Setup.java
+++ src/Floor/src/Setup.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
 public class Setup {
 	floor f1;
 	Station picker ;
 	Station packager ;
 	ChargingStation charge ;
 	m_belt f_belt ; 
+	ArrayList<ShelfArea> SA ;
 	public Setup(){
 		f1 = new floor();
 		picker = new Station(1,10);
 		packager =new Station(1,1);
 		charge = new ChargingStation(2,1);
 		f_belt = new m_belt();
+		SA = new ArrayList<ShelfArea>(); 
+		ArrayList<Integer> index = new ArrayList<Integer>();
+		index.add(1);
+		index.add(4);
+		index.add(7);
+		index.add(10);
+		for(int i = 0; i< index.size();i++){
+			int y = index.get(i);
+			ShelfArea temp = new ShelfArea(4,y,i);
+			SA.add(temp);
+		}
 	}
 }
