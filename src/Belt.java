b243d125d8fb37984fdeb90f0bbfcccd278cad2e
edvegauiowa16 <levega@yahoo.com>
2016-11-04 15:50:37 -0500

    addlocation for picker

--- src/Belt.java
+++ src/Belt.java
@@ -1,18 +1,49 @@
 import java.util.ArrayList;
 
-public class Belt 
+/**
+ * Created by Eduardo on 11/3/16.
+ * Group B4
+ */
+///needtorun = true  robot parked at picker station
+
+public class Belt implements Observer
 {
 	public ArrayList<Integer> shippedItemIDList;
+	public int x_loc_pick;
+	public int y_loc_pick;
 	
-	public void pack(Integer OrderID)//receive from picker and send to packer
+	//receives item from Robot, and sends to belt to reach packer
+	public void pack(Integer OrderID)  //receive from picker and send to packer
 	{
 		ship(OrderID);
-		
 	}
 	
-	public void ship(Integer orderID)//ships out of warehouse and store in list of shipped orders
+	//ships out of warehouse   and store in list of shipped orders
+	public void ship(Integer orderID)
 	{
 		shippedItemIDList.add(orderID);
 	}
+	
+	public void onSite_Pick(int x, int y)
+	{
+		if(x_loc_pick ==  x && y_loc_pick ==  y)
+		{
+			pack(Integer OrderID);
+		}
+	}
+	
+	@Override
+    public void update(Observable o, Object arg) 
+	{
+        //Initializes the timer
+        if (time == null) 
+        {
+            time = 0;
+        } else 
+        	{
+            	time ++;
+        	}
 
-}
+	}
+	
+}	
